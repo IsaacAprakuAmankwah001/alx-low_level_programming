454848b386cbb546c977350a69f2dd511c7b8bb4
root <isaacaprakuamankwah001>
2023-02-16 21:37:08 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a line with puts function
+ * return 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
