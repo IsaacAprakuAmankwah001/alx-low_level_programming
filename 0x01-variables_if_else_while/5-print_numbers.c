59e80145b30cb83689dbbd7bfcba85abfd63c49f
root <isaacaprakuamankwah001>
2023-02-20 18:32:46 -0800

    print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prins all strings from 0 to 9
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
+
+	return (0);
+}
