046aa05a3e04c8ca784cd3c6729314fd35487271
root <isaacaprakuamankwah001>
2023-02-20 20:12:12 -0800

    print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - printsthe numbers in base 16
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int num;
+	char la;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (la = 'a'; la <= 'f'; la++)
+		putchar(la);
+
+	putchar('\n');
+
+	return (0);
+}
