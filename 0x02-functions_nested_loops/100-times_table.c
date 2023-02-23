c24b952ff52b66b0ebe450a597406338921484d9
root <isaacaprakuamankwah001>
2023-02-22 21:55:13 -0800

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_times_table - print the 9 times table
+ */
+void print_times_table(void)
+{
+	int num, mult, prod;
+
+	if (num > 15 || n < 0)
+		printf('\n');
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
